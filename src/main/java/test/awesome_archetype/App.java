fdcef29be7c07b9dd01dffb4ea066d6b37139eed
demondevilhades <demonhades@foxmail.com>
2021-01-29 02:35:00 +0800

    build awesome-archetype

---
+++ src/main/java/test/awesome_archetype/App.java
@@ -0,0 +1,12 @@
+package test.awesome_archetype;
+
+/**
+ * 
+ * @author awesome
+ */
+public class App {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
